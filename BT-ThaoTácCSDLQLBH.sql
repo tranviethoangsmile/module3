644199be806d579ddae732598b56c2927cb5a0c0
Trần Viết Hoàng <81605215+tranviethoangsmile@users.noreply.github.com>
2021-09-15 10:07:22 +0700

    uopdate

---
+++ BT-ThaoTácCSDLQLBH.sql
@@ -0,0 +1,12 @@
+USE QuanLyBanHang;
+#Hiển thị các thông tin  gồm oID, oDate, oPrice của tất cả các hóa đơn trong bảng Order
+SELECT oID, oDATE , oTotalPrice FROM Oder;
+#Hiển thị danh sách các khách hàng đã mua hàng, và danh sách sản phẩm được mua bởi các khách
+SELECT Cus.cName AS 'Tên Khách Hàng', pr.pName AS 'Tên sản phẩm'   FROM Customer AS Cus,Product AS pr, OrderDetail AS Ord, Oder As Od
+WHERE Od.cID = Cus.cID AND Od.oID = Ord.oID;
+#Hiển thị tên những khách hàng không mua bất kỳ một sản phẩm nào
+SELECT cus.cName AS 'Khách hàng không mua' FROM Customer AS cus, Oder AS od
+WHERE NOT cus.cID = od.cID;
+#Hiển thị mã hóa đơn, ngày bán và giá tiền của từng hóa đơn (giá một hóa đơn được tính bằng tổng giá bán của từng loại mặt hàng xuất hiện trong hóa đơn. Giá bán của từng loại được tính = odQTY*pPrice)
+SELECT OD.oID, OD.oDate, P.pPRICE * ODD.odQTY  FROM Oder AS OD, OrderDetail AS ODD, Product AS P
+WHERE OD.oID = ODD.oID
